602735181222e38acd40a40b910cae0a1eb52f30
mendi <vania.unal@gmai.com>
2019-02-13 18:25:12 -0500

    Template
    Se agrega el archivo con el template del código para la realización de
    los gráficos. Los siguientes pasos son cambiar color u otras
    características.

---
+++ Template ColDep Arreglos.R
@@ -0,0 +1,63 @@
+
+rm(list=ls(all=TRUE))
+options(encoding = "UTF-8")  
+library(sp)
+library(RColorBrewer)
+library(ggplot2)
+library(maptools)
+library(scales)
+setwd("/Users/ivanmendivelso/Desktop/COL_adm")
+ohsCol2 <- readShapeSpatial("COL_adm1.shp")
+ohsColI2 <- fortify(ohsCol2)
+grupo2 <- data.frame(id = unique(ohsColI2[ , c("id")]))
+deptos <- read.csv('datos departamentos.csv', sep = ';')
+
+dim(deptos)
+
+
+
+
+grupo2[ , "Porcentaje"] <- deptos
+rownames(grupo2) <- c('Antioquia',
+                   'Atlántico',
+                   'Bolívar',
+                   'Boyacá',
+                   'Caldas',
+                   'CAQUETÁ',
+                   'Cauca',
+                   'Cesar',
+                   'Córdoba',
+                   'Cundinamarca',
+                   'Chocó',
+                   'Huila',
+                   'La Guajira',
+                   'Magdalena',
+                   'Meta',
+                   'NARIÑO',
+                   'Norte de Santander',
+                   'Quindío',
+                   'Risaralda',
+                   'Santander',
+                   'Sucre',
+                   'Tolima',
+                   'Valle del Cauca',
+                   'Arauca',
+                   'Casanare',
+                   'Putumayo',
+                   'San Andrés',
+                   'Amazonas',
+                   'Guainía',
+                   'Guaviare',
+                   'Vaupés',
+                   'Vichada')
+        
+ohsColI2 <- merge(ohsColI2, grupo2, by = "id")
+mapColDep <- ggplot() + geom_polygon(data=ohsColI2, aes(x=long, y=lat, group = group, fill = Porcentaje), colour ="white", size = 0.3) + labs(title = "Colombia", fill = "") + labs(x="",y="",title="EXPOSITORES POR DEPARTAMENTO") + scale_x_continuous(limits=c(-80,-65)) + scale_y_continuous(limits=c(-5,13))
+mapColDep
+
+ggsave(mapColDep, file = "Rural.png", width = 5, height = 4.5, type = "cairo-png")
+getwd
+ls()
+help(aes)
+
+
